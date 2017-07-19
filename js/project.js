5c480ee52782fd43801619f8aab808bce874267f
Luisa Baeta <luisabaeta@gmail.com>
2017-07-19 19:12:52 -0400

    Updating gh-pages from master branch via GitBot

---
+++ js/project.js
@@ -0,0 +1,42 @@
+$(".thumbnail").on("click", function(){
+	var gallery = $(this).data("gallery");
+	$(".gallery-wrapper").addClass("hidden");
+	$("."+gallery).removeClass("hidden");
+});
+
+$(".close").on("click", function(){
+	var gallery = $(this).data("gallery");
+	$(".gallery-wrapper").addClass("hidden");
+});
+
+// *************
+// THIS NEXT PART I ADAPTED FROM HERE: 
+// https://www.w3schools.com/howto/howto_js_slideshow.asp
+// *************
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  // var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  // for (i = 0; i < dots.length; i++) {
+  //     dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex-1].style.display = "block"; 
+  // dots[slideIndex-1].className += " active";
+}
